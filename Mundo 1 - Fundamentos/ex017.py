160fd7b6ad55c67ea79a08f07c8d09c737801fdf
Thiago Baader <thiago_andre_baader@hotmail.com>
2021-06-24 16:07:47 -0300

    Create ex017.py

---
+++ Mundo 1 - Fundamentos/ex017.py
@@ -0,0 +1,12 @@
+"""
+EXERCÍCIO 017: Catetos e Hipotenusa
+Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente
+de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
+"""
+
+from math import hypot
+
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+h = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}.'.format(h))
