a17bed527338e1bd6e05e95dd0fd013cbb096092
Thiago Baader <thiago_andre_baader@hotmail.com>
2022-01-05 15:06:29 -0300

    Update ex036.py

--- Mundo 2 - Estruturas de Controle/ex036.py
+++ Mundo 2 - Estruturas de Controle/ex036.py
@@ -1,7 +1,9 @@
+"""
 EXERCÍCIO 036: Aprovando Empréstimo
 Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
 Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 A prestação mensal não pode exceder 30% do salário, ou então o empréstimo será negado.
+"""
 
 valor = float(input('Valor da casa: R$ '))
 salario = float(input('Salário do comprador: R$ '))
