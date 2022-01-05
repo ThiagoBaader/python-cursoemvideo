9f0d0c59891c35cadfb3def9397c5583df583f56
Thiago Baader <thiago_andre_baader@hotmail.com>
2022-01-05 15:07:07 -0300

    Update ex038.py

--- Mundo 2 - Estruturas de Controle/ex038.py
+++ Mundo 2 - Estruturas de Controle/ex038.py
@@ -1,8 +1,10 @@
+"""
 EXERCÍCIO 038: Comparando Números
 Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
 - O primeiro valor é maior.
 - O segundo valor é maior.
 - Não existe valor maior, os dois são iguais.
+"""
 
 n1 = float(input('Primeiro número: '))
 n2 = float(input('Segundo número: '))
