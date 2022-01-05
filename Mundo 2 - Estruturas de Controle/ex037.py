fbb33e833ae170b30c263465aeae023600804bd0
Thiago Baader <thiago_andre_baader@hotmail.com>
2022-01-05 15:06:50 -0300

    Update ex037.py

--- Mundo 2 - Estruturas de Controle/ex037.py
+++ Mundo 2 - Estruturas de Controle/ex037.py
@@ -1,9 +1,11 @@
+"""
 EXERCÍCIO 037: Conversor de Bases Numéricas
 Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será
 a base de conversão:
 - 1 para Binário
 - 2 para Octal
 - 3 para Hexadecimal
+"""
 
 n = int(input('Digite um número inteiro: '))
 print('''Escolha uma das bases para conversão:
