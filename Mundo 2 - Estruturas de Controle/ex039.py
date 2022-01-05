f301e9519ca78809f866d6924f7d11384753ba63
Thiago Baader <thiago_andre_baader@hotmail.com>
2022-01-05 15:05:14 -0300

    Create ex039.py

---
+++ Mundo 2 - Estruturas de Controle/ex039.py
@@ -0,0 +1,18 @@
+EXERCÍCIO 039: Alistamento Militar
+Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade,
+se ele ainda vai se alistar ao serviço militar, se é a hora de se alistar, ou se já passou
+do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
+from datetime import date
+nasc = int(input('Ano de nascimento: '))
+atual = date.today().year
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
+if idade > 18:
+    print('Você já deveria ter se alistado há {} anos.'.format(idade - 18))
+    print('Seu alistamento foi em {}.'.format(nasc + 18))
+elif idade == 18:
+    print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade < 18:
+    print('Ainda faltam {} anos para o alistamento.'.format(18 - idade))
+    print('Seu alistamento será em {}.'.format(nasc + 18))
