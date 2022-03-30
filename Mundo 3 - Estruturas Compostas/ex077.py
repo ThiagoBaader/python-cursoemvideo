3d6b23b58317719d9868b0fb283b7753e475d128
Thiago Baader <thiago_andre_baader@hotmail.com>
2022-03-30 08:51:17 -0300

    Create ex077.py

---
+++ Mundo 3 - Estruturas Compostas/ex077.py
@@ -0,0 +1,14 @@
+'''
+Crie um programa que tenha uma tupla com várias palavras
+(não usar acentos).
+Depois disso, você deve mostrar, para cada palavra,
+quais são as suas vogais
+'''
+
+palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO',
+            'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+for p in palavras:
+    print(f'\nNa palavra {p} temos as vogais: ', end=' ')
+    for letra in p:
+        if letra in 'AEIOU':
+            print(letra, end=' ')
