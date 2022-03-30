7cbcc504bd71f54d848f7a648a81e52520f59ad1
Thiago Baader <thiago_andre_baader@hotmail.com>
2022-03-30 09:42:42 -0300

    Create teste.py

---
+++ Mundo 3 - Estruturas Compostas/ex108/teste.py
@@ -0,0 +1,10 @@
+"""
+Adapte o código do desafio 107, criando uma função adicional chamada 'moeda()' que consiga mostrar os valores como um valor monetário formatado.
+"""
+
+from Ex108 import moeda
+
+preco = float(input('Digite o preço: R$ '))
+print(f'A metade de {moeda.moeda(preco)} é {moeda.moeda(moeda.metade(preco))}')
+print(f'O dobro de {moeda.moeda(preco)} é {moeda.moeda(moeda.dobro(preco))}')
+print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(preco, 10))}')
