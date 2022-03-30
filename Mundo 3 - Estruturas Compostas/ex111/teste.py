07b0ccb2bacf4edec5d77d952a375b2d066bead4
Thiago Baader <thiago_andre_baader@hotmail.com>
2022-03-30 09:51:55 -0300

    Create teste.py

---
+++ Mundo 3 - Estruturas Compostas/ex111/teste.py
@@ -0,0 +1,9 @@
+"""
+Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado.
+Transfira todas as funções utilizadas nos desafios 107, 108, 109 e 110 para o primeiro pacote e mantenha tudo funcionando.
+"""
+
+from Ex111.utilidadesCeV import moeda
+
+preco = float(input('Digite o preço: R$ '))
+moeda.resumo(preco, 35, 22)
