2e55a94756fc8a9e9b1825b0e9311e285d07adff
Thiago Baader <thiago_andre_baader@hotmail.com>
2022-03-30 09:41:19 -0300

    Create moeda.py

---
+++ Mundo 3 - Estruturas Compostas/ex107/moeda.py
@@ -0,0 +1,18 @@
+def aumentar(n, taxa):
+    resultado = n + (n * (taxa/100))
+    return resultado
+
+
+def diminuir(n, taxa):
+    resultado = n - (n * (taxa/100))
+    return resultado
+
+
+def metade(n):
+    resultado = n / 2
+    return resultado
+
+
+def dobro(n):
+    resultado = n * 2
+    return resultado
